6cc94ed03d3d9ac5bc123d4363664637095c2fe3
laurent lescene <laurent.lescene@y-nov.com>
2014-11-03 10:09:10 +0100

    header test
    debut header

--- vue/site/include/header.php
+++ vue/site/include/header.php
@@ -7,6 +7,7 @@
     <title>Impose ta connerie</title>
     <meta name="keywords" content="" />
     <meta name="description" content="" />
+    
     <link href="vue/site/css/style.css" rel="stylesheet" type="text/css" media="all" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script src="js/kickstart.js"></script> <!-- KICKSTART -->
@@ -14,34 +15,32 @@
 </head>
     
 <body>
-<div id="header-wrapper">
-	<div id="header" class="container">
-		<div id="logo">
-			<h1><a href="index.php">Impose Ta Connerie</a></h1>
-		</div>
-		<div id="menu">
-			<ul>
-				<!--<li><a href="index.php" title="">Accueil</a></li> -->
-				<li><a href="index.php?section=image" title="">Image</a></li>
-				<li><a href="index.php?section=gif"  title="">Gif</a></li>
-				<li><a href="index.php?section=video" title="">Vidéo</a></li>
-				<!--<li><a href="index.php?section=compte" title="">Compte</a></li>-->
-                
+    
+	<div id="header">
+        <div id="titre">
+            <h1><a href="index.php">Impose Ta Connerie</a></h1>
+        </div>
+        
+        <div id="menu">
+            <ul>
+                <li><a href="index.php?section=image" title="">Image</a></li>
+                <li><a href="index.php?section=gif"  title="">Gif</a></li>
+                <li><a href="index.php?section=video" title="">Vidéo</a></li>
+                <!--<li><a href="index.php?section=compte" title="">Compte</a></li>-->
+
                 <?php 
                 if(isset($_SESSION['id_utilisateur'])>0)
                 { 
                 ?>
                                 <li><a href="modele/site/deconnexion.php">Déconnection</a></li>
                                 <li><a href="index.php?section=profil">Profil</a></li>
-
-                <?php
+            </ul>
+            <?php
                 }
                 else
                 {
                 include ('vue/site/include/identification.php');
                 }
                 ?>
-			</ul>
-		</div>
+        </div>
 	</div>
-</div>
