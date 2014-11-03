652cb47e6dce7cb4f5e276792b530402f3addea7
laurent lescene <laurent.lescene@y-nov.com>
2014-11-03 18:23:01 +0100

    page image/header modif

--- vue/site/include/header.php
+++ vue/site/include/header.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html>
 
 <html>
     
@@ -14,38 +14,35 @@
     <link rel="stylesheet" href="vue/site/css/kickstart.css" media="all" /> <!-- KICKSTART -->
 </head>
 <body>
-    
-	<div id="header">
-        <div id="titre">
-            <h1><a href="index.php">Impose Ta Connerie</a></h1>
-        </div>
-        
-        <div id="menu">
-            <ul>
-                <li class="icon-camera icon-large"><a href="index.php?section=image" title="">Image</a></li>
-                <li><a href="index.php?section=gif"  title="">Gif</a></li>
-                <li><a href="index.php?section=video" title="">Vidéo</a></li>
-            
-                <!--<li><a href="index.php?section=compte" title="">Compte</a></li>-->
+    <div id="header_background">
+        <div id="header">
+            <div id="titre">
+                <h1><a href="index.php">Impose Ta Connerie</a></h1>
+            </div>
 
-                <?php 
-                if(isset($_SESSION['id_utilisateur'])>0)
-                { 
-                ?>
-                                <li><a href="modele/site/deconnexion.php">Déconnection</a></li>
-                                <li><a href="index.php?section=profil">Profil</a></li>
-            </ul>
-            <?php
-                }
-                else
-                {
-                include ('vue/site/include/identification.php');
-                }
-                ?>
-        </div>
-        
-	</div>
-    
-    <div class="notice error">Fermer ce message si vous le voyez = KickStarte Work ! <a href="http://google.fr" class="icon-remove"></a></div>
+            <div id="menu">
+                <ul>
+                    <li class="icon-camera icon-large"><a href="index.php?section=image" title="">Image</a></li>
+                    <li><a href="index.php?section=gif"  title="">Gif</a></li>
+                    <li><a href="index.php?section=video" title="">Vidéo</a></li>
 
+                    <!--<li><a href="index.php?section=compte" title="">Compte</a></li>-->
+
+                    <?php 
+                    if(isset($_SESSION['id_utilisateur'])>0)
+                    { 
+                    ?>
+                                    <li><a href="modele/site/deconnexion.php">Déconnection</a></li>
+                                    <li><a href="index.php?section=profil">Profil</a></li>
+                </ul>
+                <?php
+                    }
+                    else
+                    {
+                    include ('vue/site/include/identification.php');
+                    }
+                    ?>
+            </div>
+        </div>
+    </div>
  
--- vue/site/pages/image.php
+++ vue/site/pages/image.php
@@ -1,30 +1,29 @@
 <?php $pageactive = 'image'; ?>
 <?php include('vue/site/include/header.php');?>
 
-
+<?php 
+    while($img = $prepare_compte->fetch()) { 
+?>
     <div class="article">
-        <?php 
-        while($img = $prepare_compte->fetch()) { 
-        ?>
+        
         <div class="content_gauche">
                 <h2><?php echo $img['titre'];?></h2>
                 <p>Date: <?php echo $img['date'];?></p>
                 <p>Auteur: <?php echo $img['auteur'];?></p>
                 <img class="caption" title="<?php echo $img['nom_image'];?>" src="img/img/<?php echo $img['nom_image'];?>"/>
-               
-        
         </div>
+        
         <div class="content_droite">
             <div class="twiter">
                 <a href="http://twitter.com/share" class="twitter-share-button" data-count="vertical" data-via="Impose Ta Connerie">Tweet</a>
                 <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
             </div>
         </div>
-        <?php 
-            $compt_ann = $compt_ann + 1; } 
-        ?>
-    </div>
 
+    </div>
+<?php 
+    $compt_ann = $compt_ann + 1; } 
+?>
      
 
 
