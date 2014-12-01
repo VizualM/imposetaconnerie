376fd99d0acd9df4cc1c1e84cfa184742ddccd43
cd33 <charles.diallo@y-nov.com>
2014-12-01 11:05:47 +0100

    modif header +ajout share buttons
    Redimension du header et ajout des boutons share facebook et twitter

--- vue/site/include/header.php
+++ vue/site/include/header.php
@@ -11,7 +11,25 @@
     <link href="vue/site/css/style.css" rel="stylesheet" type="text/css" media="all" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script src="vue/site/js/kickstart.js"></script> <!-- KICKSTART -->
+    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
     <link rel="stylesheet" href="vue/site/css/kickstart.css" media="all" /> <!-- KICKSTART -->
+    <script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1494049274210999',
+      xfbml      : true,
+      version    : 'v2.2'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
 </head>
 <body>
     <div id="header_background">
@@ -25,18 +43,28 @@
                     <li><a href="index.php?section=image" title="Images" class="icon-camera-retro icon-2x"></a></li>
                     <li><a href="index.php?section=gif"  title="Gifs" class="icon-magic icon-2x"></a></li>
                     <li><a href="index.php?section=video" title="Vidéos" class="icon-facetime-video icon-2x"></a></li>
-
+                    <twitter><a href="https://twitter.com/ImposeTC" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @ImposeTC</a></twitter>
+                    <facebook><div
+                            class="fb-like"
+                            data-share="true"
+                            data-width="450"
+                            data-show-faces="true"
+                            layout=button_count>
+                    </div></facebook>
+                    
                     <?php 
                     if(isset($_SESSION['id_utilisateur'])>0)
                     { 
                     ?>
                         <li><a href="modele/site/deconnexion.php">Déconnection</a></li>
                         <li><a href="index.php?section=profil">Profil</a></li>
-                </ul>
-                <?php
+                
+                    <?php
                     }
-                    else{ include ('vue/site/include/identification.php');}
-                ?>
+                        else{ include ('vue/site/include/identification.php');}
+                    ?>
+                    
+                </ul>
             </div>
         </div>
     </div>
--- vue/site/pages/image.php
+++ vue/site/pages/image.php
@@ -20,14 +20,24 @@
             
         </div>
         
-        <div class="content_droite">
-            <div class="twitter">
-                <a href="http://twitter.com/share" class="twitter-share-button" data-count="vertical" data-via="Impose Ta Connerie">Tweet</a>
-                <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
-            </div>
+        <a class="twitter-share-button"
+           href="https://twitter.com/share">
+            Tweet
+        </a>
+        <script type="text/javascript">
+            window.twttr=(function(d,s,id){var t,js,fjs=d.getElementsByTagName(s)[0];if(d.getElementById(id))   {return}js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);return window.twttr||(t={_e:[],ready:function(f){t._e.push(f)}})}(document,"script","twitter-wjs"));
+        </script>
+    
+            
+        <div class="content">
+            <div class="facebook">
+				<iframe src="http://www.facebook.com/plugins/like.php?href=http://www.example.com/page.html&layout=box_count&show_faces=true&width=65&action=like&font=arial&colorscheme=light&height=65"
+				scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:65px; height:65px; margin-top:3px;" allowTransparency="true"></iframe>
+			</div>
         </div>
 
     </div>
+
 <?php 
     $compt_ann = $compt_ann + 1; } 
 ?>
