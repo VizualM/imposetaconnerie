6364a32fa5ae83183cb46b396db2e7091a7c809b
VizualM <maxime.epinette@y-nov.com>
2015-01-15 15:10:23 +0100

    Changement d'url
    Mise en a jour de id et mdp de la base de donné !

--- models/connexion_bdd.php
+++ models/connexion_bdd.php
@@ -3,7 +3,7 @@
 	    
 	    public static function connexion_bdd(){
 	        try {   
-	            return new PDO('mysql:host=eu-cdbr-west-01.cleardb.com;dbname=heroku_0b5041832133499', 'be4af0ffb51ffe', 'd5c7dd03', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+	            return new PDO('mysql:host=eu-cdbr-west-01.cleardb.com;dbname=heroku_8acaed026551ac4', 'bfefcb3bcbbc30', '752fd298', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 	        }
 	        catch (Exception $e) {
 	            die('Erreur : ' . $e->getMessage());
