784b3b4999b88ffc444a808e73f86a8a7412dd4b
cd33 <charles.diallo@y-nov.com>
2015-01-16 11:45:14 +0100

    utf8_encode + sociaux

--- views/show_images.php
+++ views/show_images.php
@@ -8,7 +8,7 @@
     <div class="content_gauche">
 
         <div class="info_image">
-            <h2><?php echo $image[0]['titre'];?></h2>
+            <h2><?php echo utf8_encode ($image[0]['titre']);?></h2>
             <p>Date: <?php echo $image[0]['date'];?></p>
             <p>Auteur: <?php echo $image[0]['auteur'];?></p>
         </div>
@@ -30,9 +30,9 @@
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
             <div class="fb-comments" data-href="http://developers.facebook.com/docs/plugins/comments/" data-width="530" data-numposts="4" data-colorscheme="light"></div>
-            <li class="element"><a href="http://twitter.com/share" class="twitter-share-button" data-count="horizontal" data-via="Impose Ta Connerie">Tweet</a>
-            <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script></li>
-            <li class="element"><div id="fb-root"></div><script src="http://connect.facebook.net/fr_FR/all.js#xfbml=1"></script><div class="fb-like" data-share="false" data-width="450" data-show-faces="true" layout=button_count></div></li>
+            <twitter><a href="http://twitter.com/share" class="twitter-share-button" data-count="horizontal" data-via="Impose Ta Connerie">Tweet</a>
+            <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script></twitter>
+            <facebook><div id="fb-root"></div><script src="http://connect.facebook.net/fr_FR/all.js#xfbml=1"></script><div class="fb-like" data-share="false" data-width="450" data-show-faces="true" layout=button_count></div></facebook>
         </ul>   
     </div>
     
